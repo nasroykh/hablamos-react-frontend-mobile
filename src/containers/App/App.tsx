77ffd76642c1813f7a123ae4cd31074ff5ba98c4
Nasreddine Yakhou <nasro_ykh@hotmail.com>
2020-08-02 07:57:38 +0100

    first commit, folders structure

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import classes from './App.module.css';
+
+class App extends Component {
+
+	render(){
+
+		return (
+			<div className={classes.App}>
+			
+			</div>
+		  );
+		  
+	}
+}
+
+export default App;
